43a03b84d2849c5ae3ba7a08733e8bfe3c4078d7
Cesar Pérez Fonseca <88592693+KTCL13@users.noreply.github.com>
2024-09-17 17:42:04 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,39 +1,41 @@
 const express = require("express");
 const cors = require("cors");
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 app.use(cors());
 
-const getImageAsBase64 = async (url) => {
-  const response = await axios.get(url, { responseType: 'arraybuffer' });
-  return Buffer.from(response.data, 'binary').toString('base64');
-};
+
 
 app.get("/cat-dog", async (req, res) => {
     const {textCat}=req.query
 
     let catImg = null
+    let dogImg = null
+
+    const response1 = await axios.get(`https://cataas.com/cat/says/${textCat}?fontSize=50&fontColor=red`,{ responseType: 'arraybuffer' });
+    catImg = Buffer.from(response1.data,'binary').toString('base64');
 
 
-
-    await fetch(`https://cataas.com/cat/says/${textCat}?fontSize=50&fontColor=red`)
-      .then(response => response)
-      .then(data => {
-       catImg = Buffer.from(data.url,'binary').toString('base64');
-    }); 
     
-    let dogImage = null
+    let dogImageUrl = null
     
     await fetch('https://dog.ceo/api/breeds/image/random')
       .then(response => response.json())
       .then(data => {
-        dogImage = Buffer.from(data.message,'binary').toString('base64');
+       dogImageUrl= data.message;
     });
+
+    const response2 = await axios.get(dogImageUrl,{ responseType: 'arraybuffer' });
+    dogImg = Buffer.from(response2.data,'binary').toString('base64');
+
     res.json({
       catImg: `data:image/jpeg;base64,${catImg}`,
-      dogImg: `data:image/jpeg;base64,${dogImage}`
+      dogImg: `data:image/jpeg;base64,${dogImg}`
     })
   });
 
